d04766efce6ba152b39c64558b49eb971585cc02
Brian Rose <brian@heimidal.net>
2011-08-01 16:25:27 -0600

    Init new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizfinder::Application.config.secret_token = '1c2078d48a683c952dbeaed7f3c4f09078c3042870b62bea938c72a01dfb4f813280f2c6852d3cffe8f058f54a7d426e5505f6c99ff48328df14916098457e04'
