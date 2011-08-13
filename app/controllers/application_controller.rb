cefe0f1ee809b474289a27060a23aeec9538a4ac
Brian Rose <brian@heimidal.net>
2011-08-13 02:31:27 -0600

    Add basic auth on any prod box for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_on_production
+
+  protected
+
+  def authenticate_on_production
+    return if Rails.env.development? || Rails.env.test?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "crowdscore" && password == "denver"
+    end
+  end
 end
