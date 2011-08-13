120479861bb8e327620a231e5f21d7cfe2ae848e
Brian Rose <brian@heimidal.net>
2011-08-13 02:18:02 -0600

    Add Sendgrid for production mode.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,14 @@ Crowdscore::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
