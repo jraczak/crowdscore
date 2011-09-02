76abfd740c12bf068bb755b952d315b57b79d1dd
Brian Rose <brian@heimidal.net>
2011-09-02 17:46:45 -0600

    Add some docs for DeviseHelper#devise_error_messages!.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,4 +1,14 @@
 module DeviseHelper
+  # Public: Format devise error messages in the same way we do everything else.
+  #
+  # Examples
+  #
+  #   devise_error_messages! # When devise errors are present
+  #   # => '<div class="alert-message error"><p>Name can\'t be blank</p></div>'
+  #
+  #   See ApplicationHelper#errors_for.
+  #
+  # Returns a SafeBuffer string containing an error sentence.
   def devise_error_messages!
     errors_for resource
   end
