515c2242eee3cf2053fa4f6fc040bedb3387403d
Brian Rose <brian@heimidal.net>
2011-10-19 00:45:01 -0600

    Add additional columns to venue search results.

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,2 +1,8 @@
 module VenuesHelper
+  def inline_address(venue)
+    parts = [venue.address1, venue.address2, "#{venue.city}, #{venue.state} #{venue.zip}"].reject(&:blank?)
+
+    parts.map! { |p| h(p) }
+    parts.join('<br />').html_safe
+  end
 end
