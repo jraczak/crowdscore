488a8d1aee1627a1e640fa88fe67395fdcd469a6
Brian Rose <brian@heimidal.net>
2011-10-19 18:49:52 -0600

    Configure mailer options with new domain.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Crowdscore::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'crowdscore-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'beta.crowdscoreapp.com' }
   config.action_mailer.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
