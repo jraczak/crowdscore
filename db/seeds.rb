d71e9d520cfef4a34ccc9b7c2006582e32e9502d
Brian Rose <brian@heimidal.net>
2011-10-19 22:19:04 -0600

    Add new restaurant subcats.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 default_venue_categories = {
-  "Restaurant" => ["American (New)", "American (Traditional)", "Bakery", "Barbecue", "Buffet", "Burgers", "Cafeteria", "Cajun", "Caribbean", "Chinese", "Coffee/Cafe", "Cuban", "Deli" , "Dessert", "Diner" , "Doughnut Shop", "Ethiopian", "Fast Food", "Food Stand/Cart", "French" , "German", "Greek", "Hot Dogs", "Ice Cream & Frozen Yogurt", "Indian" , "Irish", "Italian", "Jamaican", "Japanese/Sushi", "Juice & Smoothie Shop", "Korean", "Latin American", "Mongolian", "Mexican" , "Middle Eastern", "Pizza", "Sandwich Shop", "Seafood", "South American", "Spanish" , "Steakhouse", "Swedish", "Tapas", "Thai", "Vegan", "Vegetarian", "Vietnamese"],
+  "Restaurant" => ["American (New)", "American (Traditional)", "Bakery", "Barbecue", "Buffet", "Burgers", "Cafeteria", "Cajun", "Caribbean", "Chinese", "Coffee/Cafe", "Cuban", "Deli" , "Dessert", "Diner" , "Doughnut Shop", "Ethiopian", "Fast Food", "Food Stand/Cart", "French" , "German", "Greek", "Hot Dogs", "Ice Cream & Frozen Yogurt", "Indian" , "Irish", "Italian", "Jamaican", "Japanese/Sushi", "Juice & Smoothie Shop", "Korean", "Latin American", "Mongolian", "Mexican" , "Middle Eastern", "Pizza", "Sandwich Shop", "Seafood", "South American", "Spanish" , "Steakhouse", "Swedish", "Tapas", "Thai", "Vegan", "Vegetarian", "Vietnamese", "Bar Food", "Swiss", "Dim Sum", "Pan Asian/Pacific Rim", "Belgian", "Southwestern/Tex Mex", "Health Food", "Wings", "Noodle Shop", "Brazilian", "Argentinian", "British/English", "Breakfast", "Mediterranean", "Salads", "Chicken"],
   "Bar & Nightlife" => ["Beer Garden", "Brewery", "Comedy Clubs", "Dance Clubs", "Dive Bar", "Gay & Lesbian", "Hookah Bar", "Jazz & Blues", "Karaoke", "Lounge", "Music Venue", "Piano Bar", "Pool Halls", "Pubs", "Sports Bar", "Themed Bar", "Wine Bar"],
   "Personal Service" => ["Barbershop", "Day Spa", "Eyebrow Threading", "Hair Removal", "Hair Salon", "Massage Parlor", "Medical Spa", "Nail Salon", "Piercing", "Tanning", "Tattoo"],
   "Hotel & Resort"=> []
 }
 
+old_count = VenueSubcategory.count
 default_venue_categories.each do |name, subs|
   puts "Updating #{name} subcategories..."
   cat = VenueCategory.find_or_create_by_name(name)
@@ -20,6 +21,9 @@ default_venue_categories.each do |name, subs|
     cat.venue_subcategories.find_or_create_by_name(sub)
   end
 end
+new_count = VenueSubcategory.count
+
+puts "#{ new_count - old_count } subcategories were added"
 
 prompt_mappings = {
   "Restaurant" => ["Temp 1", "Temp 2", "Temp 3", "Temp 4"],
