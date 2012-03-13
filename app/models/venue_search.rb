1c15fa0e1c56a6c380c1bc142e67235195447bf1
Brian Rose <brian@heimidal.net>
2012-03-12 21:02:15 -0600

    Boost some search fields.

--- app/models/venue_search.rb
+++ app/models/venue_search.rb
@@ -10,7 +10,10 @@ class VenueSearch
     fake_distance = 20 * 0.6214 # 1.5 miles
 
     Venue.search(include: [:venue_category, :venue_subcategory]) do
-      fulltext params[:q].gsub(/[^\s\w]/, ''), minimum_match: 2
+      fulltext params[:q].gsub(/[^\s\w]/, ''), minimum_match: 2 do
+        boost_fields name: 2.0, name_without_punc: 2.0, category: 1.5, tags: 1.2
+      end
+
       paginate page: params[:page]
       order_by_geodist :location, lat, long
       with(:location).in_radius(lat, long, fake_distance)
