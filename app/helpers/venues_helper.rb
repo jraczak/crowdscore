307cfab3240f45028ba01056f2c7d0e9af949b3a
Justin Raczak <jraczak@gmail.com>
2013-04-18 13:41:48 -0700

    Add integration for Tags module. Add integration for similar venues nearby module.

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -11,4 +11,18 @@ module VenuesHelper
     "#{venue.address1} #{venue.address2} <br>
     #{venue.city}, #{venue.state} #{venue.zip}"
   end
+  
+  def get_active_tag_categories(venue)
+    @tag_categories = []
+    venue.tags.each do |t|
+      unless @tag_categories.include?(t.tag_category)
+        @tag_categories << t.tag_category
+      end
+    end
+    @tag_categories
+  end
+  
+  def get_active_tags(venue)
+    @tags = venue.tags
+  end
 end
