737f84730dfab23a5945c5a3bb868e80f255befc
Justin Raczak <jraczak@gmail.com>
2013-05-12 20:47:38 -0700

    Add description field to lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512234453) do
+ActiveRecord::Schema.define(:version => 20130513034624) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(:version => 20130512234453) do
   create_table "lists", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
   add_index "lists", ["user_id"], :name => "index_lists_on_user_id"
