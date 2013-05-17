5a3e3c94ba979313d0f1928261d01bb5f36ce3e2
Justin Raczak <jraczak@gmail.com>
2013-05-16 21:25:09 -0700

    Modify higher_scored_venues method to only return 4 venues max for venue page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -45,7 +45,7 @@ class Venue < ActiveRecord::Base
     latlon(:location) { Sunspot::Util::Coordinates.new(latitude, longitude) }
   end
   
-  def self.higher_scored_than(venue, _limit = 10)
+  def self.higher_scored_than(venue, _limit = 4)
     # Original implementation below and not scoped to location. Trying new implementation.
     #where('computed_score > ?', venue.computed_score.to_i).limit _limit
     Venue.near([venue.latitude, venue.longitude], 2).where('computed_score > ? AND venue_category_id = ?', venue.computed_score.to_i, venue.venue_category_id).limit _limit
