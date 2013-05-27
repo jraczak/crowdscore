e8522c8073b0b0ecc2d8c7255678d8f3de462520
Justin Raczak <jraczak@gmail.com>
2013-05-26 20:08:45 -0700

    Add FollowsMailer and new_follower_email template

---
+++ app/mailers/follows_mailer.rb
@@ -0,0 +1,10 @@
+class FollowsMailer < ActionMailer::Base
+  default from: "support@crowdsco.re"
+  
+  def new_follower_email(followed, follower)
+    @follower = follower
+    @followed = followed
+    mail(to: followed.email, subject: "You have a new follower on Crowdscore!")
+  end
+  
+end
