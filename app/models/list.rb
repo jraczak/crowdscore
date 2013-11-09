1ce358ffa480bea862765ba2ab889c188f21af80
Justin Raczak <jraczak@gmail.com>
2013-11-09 14:51:32 -0800

    Add by_recent scope to list model. Finish using live data for lists module on user profile. Add additional styling and positioning to user profile page.

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,8 @@ class List < ActiveRecord::Base
   has_and_belongs_to_many :venues
   attr_accessible :name, :description
 
+  scope :by_recent, order('updated_at DESC')
+  
   has_many :list_likes, dependent: :destroy
   has_many :liked_by, through: :list_likes, source: :user
   
