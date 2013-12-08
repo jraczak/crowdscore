6eb5a43b65a5e60cfef188f4e5740891488be1ab
Justin Raczak <jraczak@gmail.com>
2013-12-08 13:34:51 -0800

    Update score association in VenueScore model. Remove score1-score4 validations

--- app/models/venue_score.rb
+++ app/models/venue_score.rb
@@ -1,9 +1,12 @@
 class VenueScore < ActiveRecord::Base
   belongs_to :venue
   belongs_to :user
+  has_many :scores
 
   validates :user, :venue, presence: true
-  validates :score1, :score2, :score3, :score4, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  
+  ## Remove the score1-score4 attributes for new venue_score architecture
+  #validates :score1, :score2, :score3, :score4, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   #validates :user_id, uniqueness: { scope: :venue_id }
 
   before_save :compute_score
