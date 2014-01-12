1533ec00f30fadcab1b509827c8ba111909b9c9e
Justin Raczak <jraczak@gmail.com>
2014-01-11 20:47:48 -0800

    Remove venue_score_id presence validation from score model

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,6 +3,6 @@ class Score < ActiveRecord::Base
 belongs_to :venue_score
 belongs_to :score_category
 
-validates_presence_of :value, :venue_score_id, :score_category_id
+validates_presence_of :value, :score_category_id
 
 end
