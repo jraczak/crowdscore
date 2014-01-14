fcba48800ea1db57601a4b1e0655579dc4413fff
Justin Raczak <jraczak@gmail.com>
2014-01-14 12:35:29 -0800

    Add user_id column to scores

---
+++ db/migrate/20140114203444_add_user_id_to_scores.rb
@@ -0,0 +1,6 @@
+class AddUserIdToScores < ActiveRecord::Migration
+  def change
+    add_column :scores, :user_id, :integer
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140112053658) do
+ActiveRecord::Schema.define(:version => 20140114203444) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(:version => 20140112053658) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "venue_score_id"
+    t.integer  "user_id"
   end
 
   create_table "tags", :force => true do |t|
