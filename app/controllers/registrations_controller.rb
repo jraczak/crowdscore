a0ba826b1a3a1ac9a9e69050db1f1eaa0f517568
Justin Raczak <jraczak@gmail.com>
2014-02-08 15:14:16 -0800

    Override registration update to allow updates without password except for password.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
   
+  def update
+    @user = User.find(current_user.id)
+    
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(params[:user])
+    else
+      params[:user].delete(:current_password)
+      @user.update_without_password(params[:user])
+    end
+    
+    if successfully_updated
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+  
   def after_sign_up_path_for(devise_resource)
     edit_user_registration_path
   end
@@ -10,6 +29,12 @@ class RegistrationsController < Devise::RegistrationsController
   
   def after_update_path_for(resource)
     dashboard_path
+  end
+  
+  private
+  
+  def needs_password?(user, params)
+    params[:user][:password].present?
   end
 
 end
