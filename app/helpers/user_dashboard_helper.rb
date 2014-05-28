b950e08dd53a32db8eec9a51a458a2d23711c3be
Justin Raczak <jraczak@gmail.com>
2014-05-27 20:19:31 -0700

    Add venue_category_id and venue_subcategory_id integer facets to venue model searchable block

--- app/helpers/user_dashboard_helper.rb
+++ app/helpers/user_dashboard_helper.rb
@@ -1,2 +1,4 @@
 module UserDashboardHelper
+
+  
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -42,6 +42,8 @@ class Venue < ActiveRecord::Base
     text(:category) { |venue| venue.full_category_name }
     text(:tips) { |venue| venue.tips.map(&:text) }
     #text(:tags) { |venue| venue.tags.map(&:full_name) }
+    integer :venue_category_id
+    integer :venue_subcategory_id
 
     latlon(:location) { Sunspot::Util::Coordinates.new(latitude, longitude) }
   end
