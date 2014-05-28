d2029f5fcd43b34b3e15a66074545ab9e1d83d50
Justin Raczak <jraczak@gmail.com>
2014-05-27 21:29:32 -0700

    Add hardcoded lat and long to test geospatial search of restuarant recommendations

--- app/helpers/user_dashboard_helper.rb
+++ app/helpers/user_dashboard_helper.rb
@@ -3,6 +3,7 @@ module UserDashboardHelper
   def get_restaurant_recommendations
     rec_search = Venue.search do
       with :venue_category_id, current_user.liked_venue_categories[1]
+      with(:location).in_radius(37.5559915, -122.2613072, 10)
     end
     @recs = rec_search.results
   end
