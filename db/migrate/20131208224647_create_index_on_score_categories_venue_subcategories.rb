8121b8438ab0b27cbfdc69cac1843b93afd8734d
Justin Raczak <jraczak@gmail.com>
2014-07-27 17:42:59 -0700

    Remove two more migrations with score categories to fix migration errors to production

--- db/migrate/20131208224647_create_index_on_score_categories_venue_subcategories.rb
+++
@@ -1,9 +0,0 @@
-class CreateIndexOnScoreCategoriesVenueSubcategories < ActiveRecord::Migration
-  def up
-    add_index :score_categories_venue_subcategories, :score_category_id, :name => "index_score_categories_venue_subcategories_on_score_category_id"
-    add_index :score_categories_venue_subcategories, :venue_subcategory_id, :name => "index_score_cats_venue_subcats_on_venue_subcategory_id"
-  end
-
-  def down
-  end
-end
