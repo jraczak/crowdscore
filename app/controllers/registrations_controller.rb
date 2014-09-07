c74c443974fbd91aa162c4ba6442df9c6b2aba60
Justin Raczak <jraczak@gmail.com>
2014-09-07 16:53:51 -0700

    Add server side Mixpanel tracking for Sign Up event. Add Mixpanel profile instantiation of new sign ups

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,21 +20,31 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    #Check to see if a user with provided email address exists
+    # Instantiate a Mixpanel tracker
+    tracker = Mixpanel::Tracker.new(ENV['MIXPANEL_PROJECT_TOKEN'])
+    # Check to see if a user with provided email address exists
     if User.find_by_email(params[:user][:email])
       @existing_user = User.find_by_email(params[:user][:email])
       #Check to see if the user was created through Devise's invite
       if @existing_user.was_invited?
-        #If the user was invited, redirect them to accept the invitation and pass the token
+        # If the user was invited, redirect them to accept the invitation and pass the token
         redirect_to accept_user_invitation_path(:invitation_token => @existing_user.invitation_token)
         flash[:notice] = "It looks like you were invited by #{User.find(@existing_user.invited_by_id).first_name}. Fill out this quick form and you'll be good to go!"
       else
-        #If the user exists but wasn't invited, remind them of their account and redirect to sign in
+        # If the user exists but wasn't invited, remind them of their account and redirect to sign in
         redirect_to new_user_session_path
         flash[:notice] = "Looks like you've already got an account. Sign in below."
       end
     else
       super
+      if resource.save
+        tracker.people.set("#{resource.id}", {
+	        '$first_name' => resource.first_name,
+	        '$email' => resource.email,
+	        '$username' => resource.username
+        })
+        tracker.track(resource.id, 'Sign Up')
+      end
     end
   end
   
