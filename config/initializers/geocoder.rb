5bb3031030d5344da2314818c2dfb45cca30b965
Justin Raczak <jraczak@gmail.com>
2014-09-16 17:48:17 -0700

    Add our Google Geocoder API access key to geocoder.rb to fix rate limit issue in venue search failing to geocode location param

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,5 +6,5 @@ Geocoder.configure(
   :lookup => :google,
   :timeout => 5,)
 
-# # to use an API key:
-# Geocoder::Configuration.api_key = "Ar9yHS5vIiJdt5tR20SqpQn6OIRwDCDCoxSlVqRmF0IILfIE-UFdM9lQRlya1YP_"
+# to use an API key:
+Geocoder::Configuration.api_key = "AIzaSyBhPFZz5fazzkwQwiexGXBTqEZ8Vj7_U7M"
