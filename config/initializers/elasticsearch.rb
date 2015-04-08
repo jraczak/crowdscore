b327207f6d398828465c06ce2e6be9626f7ab41a
Justin Raczak <jraczak@gmail.com>
2015-04-07 18:45:47 -0700

    Add missing ) in elasticsearch initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 require 'elasticsearch/model'
 
 if ENV['FOUNDELASTICSEARCH_URL']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['FOUNDELASTICSEARCH_URL'], logs: true}
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['FOUNDELASTICSEARCH_URL'], logs: true})
 end
