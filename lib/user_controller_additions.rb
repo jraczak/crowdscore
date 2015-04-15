8853de285665599fbe1839f3c3c97c700938bc50
Justin Raczak <jraczak@gmail.com>
2015-04-14 19:11:32 -0700

    Make sure user has liked venue categories before trying to add cuisines

--- lib/user_controller_additions.rb
+++ lib/user_controller_additions.rb
@@ -6,11 +6,13 @@ module UserControllerAdditions
   module ClassMethods
     def migrate_user_liked_venue_categories_to_cuisines(user)
       ids = []
-      user.liked_venue_categories["restaurant"].each do |id|
-        ids << VenueSubcategory.find(id).name
+      if user.liked_venue_categories["restaurant"]
+        user.liked_venue_categories["restaurant"].each do |id|
+          ids << VenueSubcategory.find(id).name
+        end
+        user.liked_venue_categories["restaurant"] = id
+        user.save!
       end
-      user.liked_venue_categories["restaurant"] = ids
-      user.save!
     end
   end
   
