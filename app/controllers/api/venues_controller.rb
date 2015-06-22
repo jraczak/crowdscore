1e8152f3112802e908e27c4824cf771ad3c88df8
Justin Raczak <jraczak@gmail.com>
2015-06-22 16:32:36 -0700

    Add initial show method to venues api method

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,4 +1,10 @@
 module API
   class VenuesController < Application Controller
+  
+    def show
+      venue = Venue.find(params[:id])
+      render json: venue, status: 200
+    end
+  
   end
 end
