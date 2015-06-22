a6dc59b40cd095df184937bbe3aefb0e0763b1d1
Justin Raczak <jraczak@gmail.com>
2015-06-22 16:20:57 -0700

    Add API to inflector initializer. Update venues controller in API module

---
+++ app/controllers/api/venues_controller.rb
@@ -0,0 +1,4 @@
+module API
+  class VenuesController < Application Controller
+  end
+end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,8 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'API'
+end
